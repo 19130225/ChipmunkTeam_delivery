9e6ca59d8e2273bc7c801171f474b9fee4bc373c
AnhTrungNguyen <103439199+AnhTrungNguyen@users.noreply.github.com>
2023-04-21 15:40:25 +0700

    Push Code System.

---
+++ OptimalDeliveryRoutes/src/main/java/com/project/deliveryroutes/OptimalDeliveryRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.project.deliveryroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalDeliveryRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimalDeliveryRoutesApplication.class, args);
+	}
+
+}
